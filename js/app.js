9483ecfa1e109de4b383db506caf93663306527e
Ilya Tsygankov <theycallmeilya@gmail.com>
2026-07-11 12:55:02 +0300

    feat: change shuffle

--- js/app.js
+++ js/app.js
@@ -57,11 +57,32 @@ function getTotalRounds(gamesCount) {
   return Math.log2(gamesCount);
 }
 
-function shuffle(array) {
+function mulberry32(seed) {
+  return function () {
+    let t = (seed += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  };
+}
+
+// function shuffle(array) {
+//   const result = [...array];
+
+//   for (let i = result.length - 1; i > 0; i -= 1) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [result[i], result[j]] = [result[j], result[i]];
+//   }
+
+//   return result;
+// }
+
+function shuffle(array, seed = 123456) {
+  const random = mulberry32(seed);
   const result = [...array];
 
-  for (let i = result.length - 1; i > 0; i -= 1) {
-    const j = Math.floor(Math.random() * (i + 1));
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(random() * (i + 1));
     [result[i], result[j]] = [result[j], result[i]];
   }
 
@@ -360,9 +381,20 @@ function startNewTournament() {
 
   hideError();
 
+  // state = {
+  //   round: 1,
+  //   participants: shuffle(allGames),
+  //   winners: [],
+  //   pairIndex: 0,
+  //   totalRounds: getTotalRounds(allGames.length),
+  //   isSelecting: false,
+  //   isFinished: false,
+  //   champion: null,
+  // };
+
   state = {
     round: 1,
-    participants: shuffle(allGames),
+    participants: shuffle(allGames, 123456),
     winners: [],
     pairIndex: 0,
     totalRounds: getTotalRounds(allGames.length),
